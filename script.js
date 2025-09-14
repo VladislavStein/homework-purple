1778dca72d8ef072965916fe285a97542c150b32
vladislavshp <vladislavshp@gmail.com>
2025-09-14 16:03:01 +0800

    Исправление

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hasLicence = true;
-const age = 18;
+const age = 19;
 const isDrunk = false;
 
-const canDrive = (hasLicence && age >= 18 && !isDrunk);
+const canDrive = (hasLicence && age > 18 && !isDrunk) ? 'Может' : 'Не может';
 console.log(canDrive);
