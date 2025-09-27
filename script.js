74e8ff02d9e14a9d6ce43935a6b3e816a66cf4ea
vladislavshp <vladislavshp@gmail.com>
2025-09-27 23:27:32 +0800

    7-converted 1.0

--- script.js
+++ script.js
@@ -10,22 +10,25 @@ function convert (amount, baseCurrency, convertedCurrency) {
             } else if (baseCurrency === 'eur') {
                 return amount * 1.1;
             }
+            break;
         case 'eur':
             if (baseCurrency === 'rub') {
                 return amount / 11;
             } else if (baseCurrency === 'usd') {
                 return amount / 1.1;
             }
+            break;
         case 'rub':
             if (baseCurrency === 'usd') {
                 return amount * 10;
             } else if (baseCurrency === 'eur') {
                 return amount * 11;
             }
+            break;
         default:
-            return undefined;
+            return null;
         }
     } else {
-        return undefined;
+        return null;
     }
 }
