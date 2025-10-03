0b8d9bd158d9f016dc9f6475e3fa66fafe6dad5a
vladislavshp <vladislavshp@gmail.com>
2025-10-03 17:00:35 +0800

    8-13.1

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function crypto(pass) {
     const splittedPass = pass.split('');
-    const [first, second, third, ...others] = splittedPass;
-    const result = [second, third, others, first].flat();
-    return result;
+    const first = splittedPass.slice(0, 4).reverse().join('');
+    const second = splittedPass.slice(4).reverse().join('');
+    return first + second;
 }
 
 function check(str, pass) {
-    const block1 = str.slice(0, 2).join('');
-    const block2 = str.slice(2, -1).join('');
-    const block3 = str.slice(-1).join('');
-    const result = block3 + block1 + block2;
+    const block1 = str.slice(0, 4).split('').reverse().join('');
+    const block2 = str.slice(4).split('').reverse().join('');
+    console.log(block1 + block2);
+    const result = block1 + block2;
     if (result === pass) {
         return true;
     } else {
         return false;
     }
 }
+
+console.log(check('ssapdrow', 'password'));
